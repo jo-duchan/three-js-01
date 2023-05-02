86b1674eef8912f2a17c0d12ac5a6a4958046691
jo-duchan <jo_duchan@icloud.com>
2023-05-02 17:50:34 +0900

    section1-13

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,6 +4,7 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "lil-gui";
 
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -43,14 +44,30 @@ document.body.appendChild(stats.dom);
 
 const gui = new GUI();
 const cubeFolder = gui.addFolder("cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
 cubeFolder.open();
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeRotationFolder.open();
 
-const cameraFolder = gui.addFolder("camera");
-cameraFolder.add(camera.position, "z", 0, 20);
-cameraFolder.open();
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position, "x", -10, 10);
+cubePositionFolder.add(cube.position, "y", -10, 10);
+cubePositionFolder.add(cube.position, "z", -10, 10);
+cubePositionFolder.open();
+
+const cubeScaleFolder = cubeFolder.addFolder("Scale");
+cubeScaleFolder.add(cube.scale, "x", -5, 5);
+cubeScaleFolder.add(cube.scale, "y", -5, 5);
+cubeScaleFolder.add(cube.scale, "z", -5, 5);
+cubeScaleFolder.open();
+
+cubeFolder.add(cube, "visible");
+
+// const cameraFolder = gui.addFolder("camera");
+// cameraFolder.add(camera.position, "z", 0, 20);
+// cameraFolder.open();
 
 function animate() {
   requestAnimationFrame(animate);
