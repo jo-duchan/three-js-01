d68eec36b6f7cf31e77439c7e94fc95338370d8e
jo-duchan <jo_duchan@icloud.com>
2023-06-17 23:37:33 +0900

    Section1-49 MTL Loader

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,90 +1,112 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import Stats from 'three/examples/jsm/libs/stats.module'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import Stats from "three/examples/jsm/libs/stats.module";
 
-const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
+const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
-const light = new THREE.PointLight()
-light.position.set(2.5, 7.5, 15)
-scene.add(light)
+const light = new THREE.PointLight();
+light.position.set(2.5, 7.5, 15);
+scene.add(light);
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 3
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 3;
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
-const material = new THREE.MeshNormalMaterial()
+const mtlLoader = new MTLLoader();
+mtlLoader.load(
+  "models/monkey.mtl",
+  (materials) => {
+    materials.preload();
 
-const objLoader = new OBJLoader()
-objLoader.load(
-    'models/monkey.obj',
-    (object) => {
-        // console.log(object);
-        // (object.children[0] as THREE.Mesh).material = material
-        object.traverse(function (child) {
-            if ((child as THREE.Mesh).isMesh) {
-                (child as THREE.Mesh).material = material
-            }
-        })
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
+    const objLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.load(
+      "models/monkey.obj",
+      (object) => {
+        scene.add(object);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      (error) => {
+        console.log("An error happened");
+      }
+    );
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log("An error happened");
+  }
+);
 
-objLoader.load(
-    'models/cube.obj',
-    (object) => {
-        object.position.x = -3
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
+mtlLoader.load(
+  "models/monkeyTextured.mtl",
+  (materials) => {
+    materials.preload();
 
-window.addEventListener('resize', onWindowResize, false)
+    const objLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.load(
+      "models/monkeyTextured.obj",
+      (object) => {
+        object.position.x = 2.5;
+        scene.add(object);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      (error) => {
+        console.log("An error happened");
+      }
+    );
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log("An error happened");
+  }
+);
+
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
 }
 
-const stats = new Stats()
-document.body.appendChild(stats.dom)
+const stats = new Stats();
+document.body.appendChild(stats.dom);
 
 function animate() {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
-    controls.update()
+  controls.update();
 
-    render()
+  render();
 
-    stats.update()
+  stats.update();
 }
 
 function render() {
-    renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 
-animate()
+animate();
